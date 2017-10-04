d9bbdd3abe8cf3779d508dd2ab17076f62559def
Thibault Martinez <thibault.martinez.30@gmail.com>
2017-10-05 01:39:18 +0200

    Fix codecov

--- coverage.sh
+++ coverage.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Generate initial coverage information
-lcov -c -i -d . -o .coverage.base
+# Generate baseline coverage data file
+lcov --capture --initial --directory . --output-file coverage_base.info
 
 # Generate coverage based on executed tests
-lcov -c -d . -o .coverage.run
+lcov --capture --directory . --output-file coverage_test.info
 
 # Merge coverage tracefiles
-lcov -a .coverage.base -a .coverage.run  -o .coverage.info
+lcov --add-tracefile coverage_base.info --add-tracefile coverage_test.info --output-file coverage_merge.info
 
-# Filtering, extracting project files
-lcov -e .coverage.info "`pwd`/source/*" "`pwd`/include/*" -o .coverage.info
+# Removing unwanted files from coverage
+lcov --remove coverage_merge.info "`pwd`/external/*" "`pwd`/test/*" "/include/*" "/usr/*" -o coverage.info
 
 # Upload coverage
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
