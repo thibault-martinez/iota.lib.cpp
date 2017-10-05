47fc01d295211f241c5b1b9892cf863668e04d7d
Thibault Martinez <thibault.martinez.30@gmail.com>
2017-10-05 13:06:21 +0200

    Update coverage script

--- coverage.sh
+++ coverage.sh
@@ -25,17 +25,17 @@
 #
 #
 
-# Generate baseline coverage data file
+echo "Generating baseline coverage data file."
 lcov --capture --initial --directory . --output-file coverage_base.info
 
-# Generate coverage based on executed tests
+echo "Generating coverage based on executed tests."
 lcov --capture --directory . --output-file coverage_test.info
 
-# Merge coverage tracefiles
+echo "Merging coverage tracefiles."
 lcov --add-tracefile coverage_base.info --add-tracefile coverage_test.info --output-file coverage_merge.info
 
-# Removing unwanted files from coverage
+echo "Removing unwanted files from coverage."
 lcov --remove coverage_merge.info "`pwd`/external/*" "`pwd`/test/*" "/include/*" "/usr/*" -o coverage.info
 
-# Upload coverage
+echo "Uploading coverage."
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
