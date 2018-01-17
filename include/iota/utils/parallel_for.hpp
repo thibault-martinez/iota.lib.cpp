194f03baafdaa9afe31497582ad99ebc19233eef
Thibault Martinez <thibault.martinez.30@gmail.com>
2018-01-17 23:07:43 +0100

    Multithreaded and optimized PoW

--- include/iota/utils/parallel_for.hpp
+++ include/iota/utils/parallel_for.hpp
@@ -36,9 +36,8 @@ namespace Utils {
 template <typename F>
 void
 parallel_for(int begin, int end, F fn) {
-  std::atomic<int> idx(begin);
-  int              num_cpus = std::thread::hardware_concurrency();
-  // TODO switch to std::array ?
+  std::atomic<int>               idx(begin);
+  int                            num_cpus = std::thread::hardware_concurrency();
   std::vector<std::future<void>> futures(num_cpus);
 
   for (int cpu = 0; cpu != num_cpus; ++cpu) {
@@ -50,6 +49,20 @@ parallel_for(int begin, int end, F fn) {
         fn(i);
       }
     });
+  }
+  for (int cpu = 0; cpu != num_cpus; ++cpu) {
+    futures[cpu].get();
+  }
+}
+
+template <typename F>
+void
+parallel_for(F fn) {
+  int                            num_cpus = std::thread::hardware_concurrency();
+  std::vector<std::future<void>> futures(num_cpus);
+
+  for (int cpu = 0; cpu != num_cpus; ++cpu) {
+    futures[cpu] = std::async(std::launch::async, fn, cpu);
   }
   for (int cpu = 0; cpu != num_cpus; ++cpu) {
     futures[cpu].get();
