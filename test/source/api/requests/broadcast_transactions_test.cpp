6f07c270058944be62c11d7a4b7a56d7016a4f4b
Simon Ninon <simon.ninon@gmail.com>
2018-01-21 12:38:09 -0800

    additional requests basic unit tests (broadcast_transactions, remove_neighbors)

---
+++ test/source/api/requests/broadcast_transactions_test.cpp
@@ -0,0 +1,63 @@
+//
+// MIT License
+//
+// Copyright (c) 2017-2018 Thibault Martinez and Simon Ninon
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+
+#include <gtest/gtest.h>
+
+#include <iota/api/requests/broadcast_transactions.hpp>
+
+TEST(BroadcastTransactionsRequest, CtorShouldInitFields) {
+  const IOTA::API::Requests::BroadcastTransactions res{ { "TESTA", "TESTB" } };
+
+  EXPECT_EQ(res.getTrytes(), std::vector<IOTA::Types::Trytes>({ "TESTA", "TESTB" }));
+}
+
+TEST(BroadcastTransactionsRequest, GetTrytesNonConst) {
+  IOTA::API::Requests::BroadcastTransactions res;
+
+  res.getTrytes().push_back("TEST");
+
+  EXPECT_EQ(res.getTrytes(), std::vector<IOTA::Types::Trytes>({ "TEST" }));
+}
+
+TEST(BroadcastTransactionsRequest, SetTrytes) {
+  IOTA::API::Requests::BroadcastTransactions res;
+
+  std::vector<IOTA::Types::Trytes> hashes = res.getTrytes();
+  hashes.push_back("TEST");
+  res.setTrytes(hashes);
+
+  EXPECT_EQ(res.getTrytes(), std::vector<IOTA::Types::Trytes>({ "TEST" }));
+}
+
+TEST(BroadcastTransactionsRequest, Serialize) {
+  const IOTA::API::Requests::BroadcastTransactions res{ { "TESTA", "TESTB" } };
+
+  json data;
+  res.serialize(data);
+
+  EXPECT_EQ(data["command"], "broadcastTransactions");
+  EXPECT_EQ(data["trytes"].get<std::vector<std::string>>(),
+            std::vector<IOTA::Types::Trytes>({ "TESTA", "TESTB" }));
+}
---
+++ test/source/api/requests/remove_neighbors_test.cpp
@@ -0,0 +1,60 @@
+// //
+// // MIT License
+// //
+// // Copyright (c) 2017-2018 Thibault Martinez and Simon Ninon
+// //
+// // Permission is hereby granted, free of charge, to any person obtaining a copy
+// // of this software and associated documentation files (the "Software"), to deal
+// // in the Software without restriction, including without limitation the rights
+// // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// // copies of the Software, and to permit persons to whom the Software is
+// // furnished to do so, subject to the following conditions:
+// //
+// // The above copyright notice and this permission notice shall be included in all
+// // copies or substantial portions of the Software.
+// //
+// // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// // SOFTWARE.
+// //
+// //
+//
+#include <gtest/gtest.h>
+
+#include <iota/api/requests/remove_neighbors.hpp>
+
+TEST(RemoveNeighborsRequest, CtorShouldInitFields) {
+  const IOTA::API::Requests::RemoveNeighbors req{ { "uri1", "uri2" } };
+
+  EXPECT_EQ(req.getUris(), std::vector<std::string>({ "uri1", "uri2" }));
+}
+
+TEST(RemoveNeighborsRequest, GetUrisNonConst) {
+  IOTA::API::Requests::RemoveNeighbors req{ { "uri1", "uri2" } };
+
+  req.getUris().push_back("uri3");
+
+  EXPECT_EQ(req.getUris(), std::vector<std::string>({ "uri1", "uri2", "uri3" }));
+}
+
+TEST(RemoveNeighborsRequest, SetUris) {
+  IOTA::API::Requests::RemoveNeighbors req;
+
+  req.setUris({ "uri1", "uri2" });
+
+  EXPECT_EQ(req.getUris(), std::vector<std::string>({ "uri1", "uri2" }));
+}
+
+TEST(RemoveNeighborsRequest, SerializeShouldInitJson) {
+  const IOTA::API::Requests::RemoveNeighbors req{ { "uri1", "uri2" } };
+  json                                       data;
+
+  req.serialize(data);
+
+  EXPECT_EQ(data["command"], "removeNeighbors");
+  EXPECT_EQ(data["uris"], std::vector<std::string>({ "uri1", "uri2" }));
+}
