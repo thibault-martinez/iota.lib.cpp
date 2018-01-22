028fe26baf68e2e4f6f26ae7d128c41cb8916b9d
Simon Ninon <simon.ninon@gmail.com>
2018-01-21 18:53:38 -0800

    additional basic unit tests (get_balances_and_format response, transfer model)

---
+++ test/source/api/responses/get_balances_and_format_test.cpp
@@ -0,0 +1,103 @@
+//
+// MIT License
+//
+// Copyright (c) 2017-2018 Thibault Martinez and Simon Ninon
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+
+#include <gtest/gtest.h>
+
+#include <iota/api/responses/get_balances_and_format.hpp>
+
+TEST(GetBalancesAndFormatResponse, DefaultCtorShouldInitFields) {
+  const IOTA::API::Responses::GetBalancesAndFormat res{};
+
+  EXPECT_EQ(res.getInput().size(), 0UL);
+  EXPECT_EQ(res.getTotalBalance(), 0);
+  EXPECT_EQ(res.getDuration(), 0);
+}
+
+TEST(GetBalancesAndFormatResponse, CtorShouldInitFields) {
+  const IOTA::API::Responses::GetBalancesAndFormat res(
+      { { "addr1", 1, 2, 3 }, { "addr2", 4, 5, 6 }, { "addr3", 7, 8, 9 } }, 42, 21);
+
+  ASSERT_EQ(res.getInput().size(), 3UL);
+
+  EXPECT_EQ(res.getInput()[0].getAddress(), "addr1");
+  EXPECT_EQ(res.getInput()[0].getBalance(), 1);
+  EXPECT_EQ(res.getInput()[0].getKeyIndex(), 2);
+  EXPECT_EQ(res.getInput()[0].getSecurity(), 3);
+
+  EXPECT_EQ(res.getInput()[1].getAddress(), "addr2");
+  EXPECT_EQ(res.getInput()[1].getBalance(), 4);
+  EXPECT_EQ(res.getInput()[1].getKeyIndex(), 5);
+  EXPECT_EQ(res.getInput()[1].getSecurity(), 6);
+
+  EXPECT_EQ(res.getInput()[2].getAddress(), "addr3");
+  EXPECT_EQ(res.getInput()[2].getBalance(), 7);
+  EXPECT_EQ(res.getInput()[2].getKeyIndex(), 8);
+  EXPECT_EQ(res.getInput()[2].getSecurity(), 9);
+
+  EXPECT_EQ(res.getTotalBalance(), 42);
+  EXPECT_EQ(res.getDuration(), 21);
+}
+
+TEST(GetBalancesAndFormatResponse, GetInputsNonConst) {
+  IOTA::API::Responses::GetBalancesAndFormat res;
+
+  res.getInput().push_back({ "addr1", 1, 2, 3 });
+
+  ASSERT_EQ(res.getInput().size(), 1UL);
+
+  EXPECT_EQ(res.getInput()[0].getAddress(), "addr1");
+  EXPECT_EQ(res.getInput()[0].getBalance(), 1);
+  EXPECT_EQ(res.getInput()[0].getKeyIndex(), 2);
+  EXPECT_EQ(res.getInput()[0].getSecurity(), 3);
+
+  EXPECT_EQ(res.getDuration(), 0);
+  EXPECT_EQ(res.getTotalBalance(), 0);
+}
+
+TEST(GetBalancesAndFormatResponse, SetInputs) {
+  IOTA::API::Responses::GetBalancesAndFormat res;
+
+  res.setInput({ { "addr1", 1, 2, 3 } });
+
+  ASSERT_EQ(res.getInput().size(), 1UL);
+
+  EXPECT_EQ(res.getInput()[0].getAddress(), "addr1");
+  EXPECT_EQ(res.getInput()[0].getBalance(), 1);
+  EXPECT_EQ(res.getInput()[0].getKeyIndex(), 2);
+  EXPECT_EQ(res.getInput()[0].getSecurity(), 3);
+
+  EXPECT_EQ(res.getDuration(), 0);
+  EXPECT_EQ(res.getTotalBalance(), 0);
+}
+
+TEST(GetBalancesAndFormatResponse, SetTotalBalance) {
+  IOTA::API::Responses::GetBalancesAndFormat res;
+
+  res.setTotalBalance(42);
+
+  EXPECT_EQ(res.getInput().size(), 0UL);
+  EXPECT_EQ(res.getTotalBalance(), 42);
+  EXPECT_EQ(res.getDuration(), 0);
+}
---
+++ test/source/models/transfer_test.cpp
@@ -0,0 +1,223 @@
+//
+// MIT License
+//
+// Copyright (c) 2017-2018 Thibault Martinez and Simon Ninon
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+
+#include <gtest/gtest.h>
+
+#include <iota/models/transfer.hpp>
+#include <test/utils/constants.hpp>
+
+TEST(Transfer, CtorDefault) {
+  IOTA::Models::Transfer t;
+
+  EXPECT_EQ(t.getAddress(), "");
+  EXPECT_EQ(t.getHash(), "");
+  EXPECT_EQ(t.getPersistence(), false);
+  EXPECT_EQ(t.getTimestamp(), "");
+  EXPECT_EQ(t.getValue(), 0);
+  EXPECT_EQ(t.getMessage(), "");
+  EXPECT_EQ(t.getTag(), "");
+}
+
+TEST(Transfer, CtorFull) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, ConstGetters) {
+  const IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, AddressGetterAndSetter) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  t.setAddress("edited");
+
+  EXPECT_EQ(t.getAddress(), "edited");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, HashGetterAndSetter) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  t.setHash("edited");
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "edited");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, PersistenceGetterAndSetter) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  t.setPersistence(false);
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), false);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, TimestampGetterAndSetter) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  t.setTimestamp("edited");
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "edited");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, ValueGetterAndSetter) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  t.setValue(42);
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 42);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, MessageGetterAndSetter) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  t.setMessage("edited");
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "edited");
+  EXPECT_EQ(t.getTag(), "tag");
+}
+
+TEST(Transfer, TagGetterAndSetter) {
+  IOTA::Models::Transfer t = { "ts", "addr", "hash", true, 1, "msg", "tag" };
+
+  t.setTag("edited");
+
+  EXPECT_EQ(t.getAddress(), "addr");
+  EXPECT_EQ(t.getHash(), "hash");
+  EXPECT_EQ(t.getPersistence(), true);
+  EXPECT_EQ(t.getTimestamp(), "ts");
+  EXPECT_EQ(t.getValue(), 1);
+  EXPECT_EQ(t.getMessage(), "msg");
+  EXPECT_EQ(t.getTag(), "edited");
+}
+
+TEST(Transfer, IsValid) {
+  IOTA::Models::Transfer t = { std::to_string(BUNDLE_1_TRX_1_TS),
+                               BUNDLE_1_TRX_1_ADDRESS,
+                               BUNDLE_1_TRX_1_HASH,
+                               true,
+                               BUNDLE_1_TRX_1_VALUE,
+                               BUNDLE_1_TRX_1_TAG,
+                               BUNDLE_1_TRX_1_TAG };
+
+  EXPECT_TRUE(t.isValid());
+}
+
+TEST(Transfer, IsValidInvalidAddress) {
+  IOTA::Models::Transfer t = { std::to_string(BUNDLE_1_TRX_1_TS),
+                               "abc",
+                               BUNDLE_1_TRX_1_HASH,
+                               true,
+                               BUNDLE_1_TRX_1_VALUE,
+                               BUNDLE_1_TRX_1_TAG,
+                               BUNDLE_1_TRX_1_TAG };
+
+  EXPECT_FALSE(t.isValid());
+}
+
+TEST(Transfer, IsValidInvalidMessage) {
+  IOTA::Models::Transfer t = { std::to_string(BUNDLE_1_TRX_1_TS),
+                               BUNDLE_1_TRX_1_ADDRESS,
+                               BUNDLE_1_TRX_1_HASH,
+                               true,
+                               BUNDLE_1_TRX_1_VALUE,
+                               "z&7",
+                               BUNDLE_1_TRX_1_TAG };
+
+  EXPECT_FALSE(t.isValid());
+}
+
+TEST(Transfer, IsValidInvalidTag) {
+  IOTA::Models::Transfer t = { std::to_string(BUNDLE_1_TRX_1_TS),
+                               BUNDLE_1_TRX_1_ADDRESS,
+                               BUNDLE_1_TRX_1_HASH,
+                               true,
+                               BUNDLE_1_TRX_1_VALUE,
+                               BUNDLE_1_TRX_1_TAG,
+                               "*&7" };
+
+  EXPECT_FALSE(t.isValid());
+}
+
+TEST(Transfer, IsValidInvalidTagLength) {
+  IOTA::Models::Transfer t = { std::to_string(BUNDLE_1_TRX_1_TS),
+                               BUNDLE_1_TRX_1_ADDRESS,
+                               BUNDLE_1_TRX_1_HASH,
+                               true,
+                               BUNDLE_1_TRX_1_VALUE,
+                               BUNDLE_1_TRX_1_TAG,
+                               "9" };
+
+  EXPECT_FALSE(t.isValid());
+}
