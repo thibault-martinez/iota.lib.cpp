eea7deaeba31a70fe4f3736c16a9e7529726d342
Simon Ninon <simon.ninon@gmail.com>
2018-01-21 18:01:05 -0800

    update Snapshot.java with most recent code to allow IRI to compile (new IRI release is incompatible with our altered version of Snapshot.java) update iri version (bin name) make sure to always refer the same commit when cloning IRI in testing environment

--- test/testnet/Snapshot.java
+++ test/testnet/Snapshot.java
@@ -1,10 +1,13 @@
 package com.iota.iri;
+import com.iota.iri.controllers.MilestoneViewModel;
+import com.iota.iri.controllers.StateDiffViewModel;
 import com.iota.iri.hash.Curl;
 import com.iota.iri.hash.ISS;
 import com.iota.iri.hash.Sponge;
 import com.iota.iri.hash.SpongeFactory;
 import com.iota.iri.model.Hash;
 import com.iota.iri.controllers.TransactionViewModel;
+import com.iota.iri.storage.Tangle;
 import com.iota.iri.utils.Converter;
 import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
@@ -15,6 +18,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 
@@ -26,6 +31,7 @@ public class Snapshot {
 
     public static final Map<Hash, Long> initialState = new HashMap<>();
     public static final Snapshot initialSnapshot;
+    public final ReadWriteLock rwlock = new ReentrantReadWriteLock();
 
     static {
 
@@ -47,64 +53,66 @@ public class Snapshot {
                     initialState.put(new Hash(key), Long.valueOf(value));
                 }
             }
-            //{ // Check snapshot signature
-            //    trits = new int[Curl.HASH_LENGTH];
-            //    curl.squeeze(trits, 0, Curl.HASH_LENGTH);
-            //    SpongeFactory.Mode mode = SpongeFactory.Mode.CURLP81;
-            //    int[] digests = new int[0];
-            //    int[] bundle = ISS.normalizedBundle(trits);
-            //    int[] root;
-            //    int i;
-            //    in = Snapshot.class.getResourceAsStream("/Snapshot.sig");
-            //    reader = new BufferedReader(new InputStreamReader(in));
-            //    for(i = 0; i < 3 && (line = reader.readLine()) != null; i++) {
-            //        int[] lineTrits = Converter.allocateTritsForTrytes(line.length());
-            //        Converter.trits(line, lineTrits, 0);
-            //        digests = ArrayUtils.addAll(
-            //                digests,
-            //                ISS.digest(mode
-            //                        , Arrays.copyOfRange(bundle, i*ISS.NORMALIZED_FRAGMENT_LENGTH, (i+1)*ISS.NORMALIZED_FRAGMENT_LENGTH)
-            //                        , lineTrits));
-            //    }
-            //    if((line = reader.readLine()) != null) {
-            //        int[] lineTrits = Converter.allocateTritsForTrytes(line.length());
-            //        Converter.trits(line, lineTrits, 0);
-            //        root = ISS.getMerkleRoot(mode, ISS.address(mode, digests), lineTrits, 0, SNAPSHOT_INDEX, SNAPSHOT_PUBKEY_DEPTH);
-            //    } else {
-            //        root = ISS.address(mode, digests);
-            //    }
-
-            //    int[] pubkeyTrits = Converter.allocateTritsForTrytes(SNAPSHOT_PUBKEY.length());
-            //    Converter.trits(SNAPSHOT_PUBKEY, pubkeyTrits, 0);
-            //    if(!Arrays.equals(pubkeyTrits, root)) {
-            //        throw new RuntimeException("Snapshot signature failed.");
-            //    }
-            //}
+            // { // Check snapshot signature
+            //     trits = new int[Curl.HASH_LENGTH];
+            //     curl.squeeze(trits, 0, Curl.HASH_LENGTH);
+            //     SpongeFactory.Mode mode = SpongeFactory.Mode.CURLP81;
+            //     int[] digests = new int[0];
+            //     int[] bundle = ISS.normalizedBundle(trits);
+            //     int[] root;
+            //     int i;
+            //     in = Snapshot.class.getResourceAsStream("/Snapshot.sig");
+            //     reader = new BufferedReader(new InputStreamReader(in));
+            //     for(i = 0; i < 3 && (line = reader.readLine()) != null; i++) {
+            //         int[] lineTrits = Converter.allocateTritsForTrytes(line.length());
+            //         Converter.trits(line, lineTrits, 0);
+            //         digests = ArrayUtils.addAll(
+            //                 digests,
+            //                 ISS.digest(mode
+            //                         , Arrays.copyOfRange(bundle, i*ISS.NORMALIZED_FRAGMENT_LENGTH, (i+1)*ISS.NORMALIZED_FRAGMENT_LENGTH)
+            //                         , lineTrits));
+            //     }
+            //     if((line = reader.readLine()) != null) {
+            //         int[] lineTrits = Converter.allocateTritsForTrytes(line.length());
+            //         Converter.trits(line, lineTrits, 0);
+            //         root = ISS.getMerkleRoot(mode, ISS.address(mode, digests), lineTrits, 0, SNAPSHOT_INDEX, SNAPSHOT_PUBKEY_DEPTH);
+            //     } else {
+            //         root = ISS.address(mode, digests);
+            //     }
+            //
+            //     int[] pubkeyTrits = Converter.allocateTritsForTrytes(SNAPSHOT_PUBKEY.length());
+            //     Converter.trits(SNAPSHOT_PUBKEY, pubkeyTrits, 0);
+            //     if(!Arrays.equals(pubkeyTrits, root)) {
+            //         throw new RuntimeException("Snapshot signature failed.");
+            //     }
+            // }
         } catch (IOException e) {
             System.out.println("Failed to load snapshot.");
             System.exit(-1);
         }
 
         initialSnapshot = new Snapshot(initialState, 0);
-        if(!initialSnapshot.isConsistent()) {
+        long stateValue = initialState.values().stream().reduce(Math::addExact).orElse(Long.MAX_VALUE);
+        if(stateValue != TransactionViewModel.SUPPLY) {
+            log.error("Transaction resolves to incorrect ledger balance: {}", TransactionViewModel.SUPPLY - stateValue);
+            System.exit(-1);
+        }
+
+        if(!isConsistent(initialState)) {
             System.out.println("Initial Snapshot inconsistent.");
             System.exit(-1);
         }
     }
 
-    public final Object snapshotSyncObject = new Object();
-    public final Object approvalsSyncObject = new Object();
-    public final Set<Hash> approvedHashes = new HashSet<>();
-    private final Map<Hash, Long> state;
+    protected final Map<Hash, Long> state;
     private int index;
 
     public int index() {
-        return index;
-    }
-
-    public Snapshot(Snapshot snapshot) {
-        state = new HashMap<>(snapshot.state);
-        this.index = snapshot.index;
+        int i;
+        rwlock.readLock().lock();
+        i = index;
+        rwlock.readLock().unlock();
+        return i;
     }
 
     private Snapshot(Map<Hash, Long> initialState, int index) {
@@ -112,49 +120,43 @@ public class Snapshot {
         this.index = index;
     }
 
-    public Map<Hash, Long> getState() {
-        return state;
+    public Snapshot clone() {
+        return new Snapshot(state, index);
     }
 
-    public Map<Hash, Long> diff(Map<Hash, Long> newState) {
-        return newState.entrySet().parallelStream()
-                .map(hashLongEntry ->
-                        new HashMap.SimpleEntry<>(hashLongEntry.getKey(),
-                                hashLongEntry.getValue() -
-                                        (state.containsKey(hashLongEntry.getKey()) ?
-                                                state.get(hashLongEntry.getKey()): 0) ))
-                .filter(e -> e.getValue() != 0L)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    public Long getBalance(Hash hash) {
+        Long l;
+        rwlock.readLock().lock();
+        l = state.get(hash);
+        rwlock.readLock().unlock();
+        return l;
     }
 
-    public Snapshot patch(Map<Hash, Long> diff, int index) {
-        Map<Hash, Long> patchedState = state.entrySet().parallelStream()
-                .map( hashLongEntry ->
-                        new HashMap.SimpleEntry<>(hashLongEntry.getKey(),
-                                hashLongEntry.getValue() +
-                                        (diff.containsKey(hashLongEntry.getKey()) ?
-                                         diff.get(hashLongEntry.getKey()) : 0)) )
-                .filter(e -> e.getValue() != 0L)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        diff.entrySet().stream()
-                .filter(e -> e.getValue() > 0L)
-                .forEach(e -> patchedState.putIfAbsent(e.getKey(), e.getValue()));
-        return new Snapshot(patchedState, index);
+    public Map<Hash, Long> patchedDiff(Map<Hash, Long> diff) {
+        Map<Hash, Long> patch;
+        rwlock.readLock().lock();
+        patch = diff.entrySet().stream().map(hashLongEntry ->
+            new HashMap.SimpleEntry<>(hashLongEntry.getKey(), state.getOrDefault(hashLongEntry.getKey(), 0L) + hashLongEntry.getValue())
+        ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        rwlock.readLock().unlock();
+        return patch;
     }
 
-    void merge(Snapshot snapshot) {
-        state.clear();
-        state.putAll(snapshot.state);
-        index = snapshot.index;
-    }
-
-    boolean isConsistent() {
-        long stateValue = state.values().stream().reduce(Math::addExact).orElse(Long.MAX_VALUE);
-        if(stateValue != TransactionViewModel.SUPPLY) {
-            long difference = TransactionViewModel.SUPPLY - stateValue;
-            log.info("Transaction resolves to incorrect ledger balance: {}", difference);
-            return false;
+    void apply(Map<Hash, Long> patch, int newIndex) {
+        if (!patch.entrySet().stream().map(Map.Entry::getValue).reduce(Math::addExact).orElse(0L).equals(0L)) {
+            throw new RuntimeException("Diff is not consistent.");
         }
+        rwlock.writeLock().lock();
+        patch.entrySet().stream().forEach(hashLongEntry -> {
+            if (state.computeIfPresent(hashLongEntry.getKey(), (hash, aLong) -> hashLongEntry.getValue() + aLong) == null) {
+                state.putIfAbsent(hashLongEntry.getKey(), hashLongEntry.getValue());
+            }
+        });
+        index = newIndex;
+        rwlock.writeLock().unlock();
+    }
+
+    public static boolean isConsistent(Map<Hash, Long> state) {
         final Iterator<Map.Entry<Hash, Long>> stateIterator = state.entrySet().iterator();
         while (stateIterator.hasNext()) {
 
