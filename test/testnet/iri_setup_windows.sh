1c0b335b4f7497a93d6f7985621b46f5378e168b
Simon Ninon <simon.ninon@gmail.com>
2018-02-09 00:08:45 -0800

    run both package and java jar in background?

--- test/testnet/iri_setup_windows.sh
+++ test/testnet/iri_setup_windows.sh
@@ -35,5 +35,5 @@ cp ../iri_patch/TransactionValidator.java src/main/java/com/iota/iri/Transaction
 cp ../iri_patch/TransactionValidatorTest.java src/test/java/com/iota/iri/TransactionValidatorTest.java
 cp -r ../testnetdb_windows ./testnetdb
 mvn clean compile
-mvn package &
+(mvn package && java -jar target/iri-1.4.2.1.jar -c iri.ini 1> /dev/null) &
 cd -
