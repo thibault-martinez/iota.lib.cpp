4cd08e9869ff8e86cee51885c03a86a7ef6b3627
Simon Ninon <simon.ninon@gmail.com>
2018-02-10 21:03:46 -0800

    use testnetdb_windows

--- test/testnet/iri_setup_windows.sh
+++ test/testnet/iri_setup_windows.sh
@@ -35,4 +35,4 @@ cp ../iri_patch/TransactionValidator.java src/main/java/com/iota/iri/Transaction
 cp ../iri_patch/TransactionValidatorTest.java src/test/java/com/iota/iri/TransactionValidatorTest.java
 mvn clean compile
 mvn package
-cp -r ../testnetdb ./testnetdb
+cp -r ../testnetdb_windows ./testnetdb
