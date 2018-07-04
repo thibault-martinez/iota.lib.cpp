e3b267b72bef936c3e3b6d3320b96368fef812a9
Thibault Martinez <thibault.martinez.30@gmail.com>
2018-07-04 18:04:07 +0800

    Dump logs

--- test/testnet/scripts/iri_setup.sh
+++ test/testnet/scripts/iri_setup.sh
@@ -32,6 +32,9 @@ cp ../iri_config/Snapshot.txt src/main/resources
 cp ../iri_config/iri.ini .
 mvn clean compile
 mvn package
+echo "BEFORE"
+cat /Users/travis/build/thibault-martinez/iota.lib.cpp/test/testnet/iri/target/surefire-reports/*
+echo "AFTER"
 cp -r ../testnetdb .
 java -jar target/iri-1.4.2.4.jar --testnet --testnet-no-coo-validation --snapshot=src/main/resources/Snapshot.txt -p 14265 --mwm 1 -c iri.ini &
 cd -
